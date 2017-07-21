82bc2b987cb5809370df362e1398ed25a249f9e1
Cüneyt Yılmaz <cuneyilmaz@gmail.com>
2017-07-22 00:54:35 +0300

    Webpack, babelrc and sample js files added

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    devServer: {
+        inline: true,
+        contentBase: './src',
+        port: 3000
+    },
+    devtool: 'cheap-module-eval-source-map',
+    entry: './dev/js/index.js',
+    resolveLoader: {
+    moduleExtensions: ['-loader']
+   },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loaders: ['babel'],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.scss/,
+                loader: 'style-loader!css-loader!sass-loader'
+            }
+        ]
+    },
+    output: {
+        path: path.resolve(__dirname, 'src'),
+        filename: 'js/bundle.min.js'
+    },
+    plugins: [
+        new webpack.optimize.OccurrenceOrderPlugin()
+    ]
+};
