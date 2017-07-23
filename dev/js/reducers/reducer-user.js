59727a2337e9446a2d2fe7375c61df4f90e0db27
Cüneyt Yılmaz <cuneyilmaz@gmail.com>
2017-07-23 04:25:01 +0300

    Finishing up

--- dev/js/reducers/reducer-user.js
+++ dev/js/reducers/reducer-user.js
@@ -12,17 +12,19 @@ export default function() {
       id: 2,
       first: "Joby",
       last: "Wasilenko",
-      age: 27,
+      age: 45,
       description: "Joby loves the Packers, cheese and turtles.",
-      thumbnail: "http://i.imgur.com/52xRlm8.png"
+      thumbnail:
+        "https://images-na.ssl-images-amazon.com/images/M/MV5BNjk0ODIwNDczOF5BMl5BanBnXkFtZTcwMzQ0NTQ0NA@@._V1_UY317_CR2,0,214,317_AL_.jpg"
     },
     {
       id: 3,
       first: "Madison",
       last: "Williams",
-      age: 30,
+      age: 23,
       description: "Madi likes her dog but it is really annoying.",
-      thumbnail: "http://i.imgur.com/4EMtxHB.png"
+      thumbnail:
+        "https://pbs.twimg.com/profile_images/864654172708233218/ZuOVYc8V.jpg"
     }
   ];
 }
